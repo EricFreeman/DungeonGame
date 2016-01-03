f49a1de27b76e6d9786ce0e2b84cbe184c0f9dcf
Eric Freeman <clownhunter@gmail.com>
2016-01-02 20:32:55 -0700

    Lock mouse position now too.

---
+++ src/Assets/Resources/Scripts/Player/LockMouse.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Resources.Scripts.Player
+{
+    public class LockMouse : MonoBehaviour
+    {
+        void Update()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit();
+
+                #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+                #endif
+            }
+        }
+    }
+}
