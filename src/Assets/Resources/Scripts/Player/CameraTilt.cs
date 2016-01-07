aa66830c52351514d741ee76cd537b527aa0d52c
Eric Freeman <clownhunter@gmail.com>
2016-01-07 00:15:17 -0700

    Added head tilt.

---
+++ src/Assets/Resources/Scripts/Player/CameraTilt.cs
@@ -0,0 +1,25 @@
+﻿using Assets.Resources.Scripts.Utils;
+using UnityEngine;
+
+namespace Assets.Resources.Scripts.Player
+{
+    public class CameraTilt : MonoBehaviour
+    {
+        public float MaxTilt = 2;
+        public float TiltSpeed = 1f;
+
+        private float _currentTilt;
+
+        
+        void Update()
+        {
+            var sideMovemnt = Input.GetAxisRaw("Horizontal");
+            var tilt = -1 * sideMovemnt * MaxTilt;
+
+            _currentTilt = _currentTilt.MoveTowards(tilt, TiltSpeed);
+
+            var rot = transform.localRotation;
+            transform.localRotation = Quaternion.AngleAxis(_currentTilt, Vector3.forward);
+        }
+    }
+}
