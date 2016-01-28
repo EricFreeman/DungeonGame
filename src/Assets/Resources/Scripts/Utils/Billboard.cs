08fdc75f8c713a65c3aadfc5dd8fc1cc95aba1c7
Eric Freeman <clownhunter@gmail.com>
2016-01-27 19:23:33 -0700

    Added bilboard effect to enemies.

---
+++ src/Assets/Resources/Scripts/Utils/Billboard.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets.Resources.Scripts.Utils
+{
+    public class Billboard : MonoBehaviour
+    {
+        private void Update()
+        {
+////            var x = transform.localRotation.x;
+//            transform.LookAt(UnityEngine.Camera.main.transform.position);
+//            transform.rotation = new Quaternion(0f, transform.rotation.y, 0f, 0f);
+
+            Vector3 v = UnityEngine.Camera.main.transform.position - transform.position;
+            v.x = v.z = 0.0f;
+            transform.LookAt(UnityEngine.Camera.main.transform.position - v);
+        }
+    }
+}
