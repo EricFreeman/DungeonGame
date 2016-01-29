c1cc5da2e537421132f614016adf17dfd9e1a60d
Eric Freeman <clownhunter@gmail.com>
2016-01-28 20:29:24 -0700

    Removed commented out code that I forgot to before commiting last.

--- src/Assets/Resources/Scripts/Utils/Billboard.cs
+++ src/Assets/Resources/Scripts/Utils/Billboard.cs
@@ -6,11 +6,7 @@ namespace Assets.Resources.Scripts.Utils
     {
         private void Update()
         {
-////            var x = transform.localRotation.x;
-//            transform.LookAt(UnityEngine.Camera.main.transform.position);
-//            transform.rotation = new Quaternion(0f, transform.rotation.y, 0f, 0f);
-
-            Vector3 v = UnityEngine.Camera.main.transform.position - transform.position;
+            var v = UnityEngine.Camera.main.transform.position - transform.position;
             v.x = v.z = 0.0f;
             transform.LookAt(UnityEngine.Camera.main.transform.position - v);
         }
