8d6bb4fc62326b1ba1bacc2400c3ab1efeee0478
Eric Freeman <efreeman@1800contacts.com>
2016-02-18 16:51:07 -0700

    I dunno, just backing stuff up.

---
+++ src/Assets/Scripts/Enemy/EnemyRangedAttack.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Scripts.Player;
+using Assets.Scripts.Weapons;
+using UnityEngine;
+
+namespace Assets.Scripts.Enemy
+{
+    public class EnemyRangedAttack : MonoBehaviour
+    {
+        public GameObject Projectile;
+        public GameObject Tip;
+        public float MinDistance = .9f;
+        public int Damage = 5;
+
+        private PlayerHealth _player;
+
+        private void Start()
+        {
+            _player = GameObject.Find("Player").GetComponent<PlayerHealth>();
+        }
+
+        public void Attack()
+        {
+            var projectile = Instantiate(Projectile);
+            projectile.transform.position = Tip.transform.position;
+            projectile.transform.rotation = Tip.transform.rotation;
+            projectile.GetComponent<Bullet>().IsFriendly = false;
+        }
+    }
+}
