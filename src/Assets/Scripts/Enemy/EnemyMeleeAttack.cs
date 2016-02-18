e4908fd9c4d5a9cf985a5ca25c4b63bb68f19fbb
Eric Freeman <clownhunter@gmail.com>
2016-02-17 23:34:24 -0700

    Enemies can damage player now.

---
+++ src/Assets/Scripts/Enemy/EnemyMeleeAttack.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Scripts.Player;
+using UnityEngine;
+
+namespace Assets.Scripts.Enemy
+{
+    public class EnemyMeleeAttack : MonoBehaviour
+    {
+        public float MinDistance = .9f;
+        public int Damage = 5;
+
+        private PlayerHealth _player;
+
+        private void Start()
+        {
+            _player = GameObject.Find("Player").GetComponent<PlayerHealth>();
+        }
+
+        public void Attack()
+        {
+            if (Vector3.Distance(transform.position, _player.transform.position) < MinDistance)
+            {
+                _player.TakeDamage(Damage);
+            }
+        }
+    }
+}
---
+++ src/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Player
+{
+    public class PlayerHealth : MonoBehaviour
+    {
+        public int Health;
+        public Text HealthTextBox;
+
+        void Update()
+        {
+            HealthTextBox.text = "Health: " + Health;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+        }
+    }
+}
